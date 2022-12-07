b13ce7a94b11d48d60d966042bb1650886d3cac0
0Chord <seaweed.0chord@gmail.com>
2022-12-07 14:14:52 +0900

    8081포트 API통신을 통한 회원가입 처리 기능 추가

--- sql/SQL.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE member(
-    id int NOT NULL auto_increment primary key,
-    user_id varchar(20) not null,
-    user_password varchar(100) not null,
-    user_nickname varchar(20) not null,
-    user_tell varchar(20) not null,
-    user_name varchar(10) not null,
-    admin_right boolean not null default 0
-);
--- src/main/java/authenticationSystem/authenticationSystem/controller/SignupController.java
+++ src/main/java/authenticationSystem/authenticationSystem/controller/SignupController.java
@@ -2,9 +2,13 @@ package authenticationSystem.authenticationSystem.controller;
 
 import authenticationSystem.authenticationSystem.dto.MemberForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,8 +34,17 @@ public class SignupController {
 
     @PostMapping("/enroll")
     public String enroll(@Validated MemberForm memberForm){
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        restTemplate.postForEntity("http://localhost:8081/signup/enroll",)
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("userId",memberForm.getUserId());
+        body.add("password", memberForm.getPassword());
+        body.add("authPassword", memberForm.getAuthPassword());
+        body.add("name", memberForm.getName());
+        body.add("nickname", memberForm.getNickname());
+        body.add("phone", memberForm.getPhone());
+        HttpEntity<MultiValueMap<String, String>> requestMessage = new HttpEntity<>(body, httpHeaders);
+        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/signup/enroll", requestMessage, String.class);
+        System.out.println("response = " + response);
         return "redirect:/";
     }
 }
