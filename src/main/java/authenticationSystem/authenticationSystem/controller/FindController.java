f38faac331de75e21ceb80f1548eb4b68591109a
0Chord <seaweed.0chord@gmail.com>
2022-12-20 03:28:05 +0900

    비밀번호 찾기 기능 추가

--- src/main/java/authenticationSystem/authenticationSystem/controller/FindController.java
+++ src/main/java/authenticationSystem/authenticationSystem/controller/FindController.java
@@ -36,32 +36,32 @@ public class FindController {
     }
 
     @GetMapping("/password")
-    public String findPassword(@ModelAttribute FindPasswordDto findPasswordDto){
+    public String findPassword(@ModelAttribute FindPasswordDto findPasswordDto) {
         return "/find/password";
     }
 
     @PostMapping("/password")
-    public String find(@ModelAttribute @Validated FindPasswordDto findPasswordDto, BindingResult bindingResult){
+    public String find(@ModelAttribute @Validated FindPasswordDto findPasswordDto, BindingResult bindingResult) {
         httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("userId",findPasswordDto.getUserId());
-        body.add("name",findPasswordDto.getName());
-        body.add("phone",findPasswordDto.getPhone());
+        body.add("userId", findPasswordDto.getUserId());
+        body.add("name", findPasswordDto.getName());
+        body.add("phone", findPasswordDto.getPhone());
         HttpEntity<MultiValueMap<String, String>> requestMessage = new HttpEntity<>(body, httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/find/password", requestMessage, String.class);
-        try{
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/find/password", requestMessage, String.class);
             String responseBody = response.getBody();
-            if(Objects.equals(responseBody, "NoSearchMember")){
-                bindingResult.reject("searchFail","아이디가 일치하지 않습니다.");
+            if (Objects.equals(responseBody, "NoSearchMember")) {
+                bindingResult.reject("searchFail", "아이디가 일치하지 않습니다.");
                 return "find/password";
             } else if (Objects.equals(responseBody, "DifferentName")) {
-                bindingResult.reject("searchFail","이름이 일치하지 않습니다.");
+                bindingResult.reject("searchFail", "이름이 일치하지 않습니다.");
                 return "find/password";
             } else if (Objects.equals(responseBody, "DifferentPhone")) {
-                bindingResult.reject(responseBody,"전화번호가 일치하지 않습니다.");
+                bindingResult.reject(responseBody, "전화번호가 일치하지 않습니다.");
                 return "find/password";
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             return "error/searchErrorPage";
         }
         authService.passwordAuth(httpHeaders, restTemplate, findPasswordDto.getUserId());
@@ -70,7 +70,7 @@ public class FindController {
     }
 
     @GetMapping("/success")
-    public String success(){
+    public String success() {
         return "redirect:/";
     }
 }
--- src/main/java/authenticationSystem/authenticationSystem/controller/SignInController.java
+++ src/main/java/authenticationSystem/authenticationSystem/controller/SignInController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +22,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.Objects;
+
 @Slf4j
 @Controller
 @RequestMapping("/signIn")
@@ -43,7 +45,7 @@ public class SignInController {
         body.add("userId", loginForm.getId());
         body.add("password", loginForm.getPassword());
         HttpEntity<MultiValueMap<String, String>> requestMessage = new HttpEntity<>(body, httpHeaders);
-        try{
+        try {
             ResponseEntity<JwtForm> response = restTemplate.postForEntity("http://localhost:8081/signIn/login", requestMessage, JwtForm.class);
             JwtForm responseBody = response.getBody();
             assert responseBody != null;
@@ -55,10 +57,10 @@ public class SignInController {
             HttpHeaders httpHeaders1 = new HttpHeaders();
             httpHeaders1.setLocation(URI.create("/signIn/auth"));
             return new ResponseEntity<>(responseBody, httpHeaders1, HttpStatus.SEE_OTHER);
-        }catch(Exception e){
+        } catch (Exception e) {
             HttpHeaders httpHeaders1 = new HttpHeaders();
             httpHeaders1.setLocation(URI.create("/Home"));
-            return new ResponseEntity<>("loginError",httpHeaders1,HttpStatus.SEE_OTHER);
+            return new ResponseEntity<>("loginError", httpHeaders1, HttpStatus.SEE_OTHER);
         }
 
     }
@@ -73,26 +75,82 @@ public class SignInController {
             return "error/accessErrorPage";
         }
         String refreshToken = authService.findAccessTokenAndRefreshToken(body, cookies);
-            ResponseEntity<?> response = authService.checkAccessToken(refreshToken, httpServletResponse, body);
-            String admin = authService.findAdmin(httpHeaders, restTemplate, body);
-            if (Boolean.TRUE.equals(response.getBody())) {
-                if (admin.equals("ROLE_ADMIN")) {
-                    ResponseEntity<?> members = authService.getMembers(httpHeaders, restTemplate);
-                    model.addAttribute("members",members.getBody());
-                    return "signIn/manage";
-                } else {
-                    ResponseEntity<?> memberInfo = authService.getMemberInfo(refreshToken, httpHeaders, restTemplate);
-                    Object member = memberInfo.getBody();
-                    model.addAttribute("member",member);
-                    return "signIn/private";
-                }
+        ResponseEntity<?> response = authService.checkAccessToken(refreshToken, httpServletResponse, body);
+        String admin = authService.findAdmin(httpHeaders, restTemplate, body);
+        if (Boolean.TRUE.equals(response.getBody())) {
+            if (admin.equals("ROLE_ADMIN")) {
+                ResponseEntity<?> members = authService.getMembers(httpHeaders, restTemplate);
+                model.addAttribute("members", members.getBody());
+                return "signIn/manage";
             } else {
-                return "Home";
+                ResponseEntity<?> memberInfo = authService.getMemberInfo(refreshToken, httpHeaders, restTemplate);
+                Object member = memberInfo.getBody();
+                model.addAttribute("member", member);
+                return "signIn/private";
             }
+        } else {
+            return "Home";
+        }
     }
 
     @GetMapping("/confirmLogin")
-    public String confirmLogin(){
+    public String confirmLogin(@ModelAttribute LoginForm loginForm, HttpServletRequest request, HttpServletResponse httpServletResponse) {
+        Cookie[] cookies = request.getCookies();
 
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        if (cookies == null) {
+            return "error/accessErrorPage";
+        }
+        String refreshToken = authService.findAccessTokenAndRefreshToken(body, cookies);
+        ResponseEntity<?> response = authService.checkAccessToken(refreshToken, httpServletResponse, body);
+        if (Boolean.TRUE.equals(response.getBody())) {
+           return "signIn/confirmLogin";
+        } else {
+            return "error/searchErrorPage";
+        }
+    }
+
+    @PostMapping("/confirmLogin")
+    public String confirm(@ModelAttribute @Validated LoginForm loginForm, Model model, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "error/searchErrorPage";
+        }
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("userId", loginForm.getId());
+        body.add("password", loginForm.getPassword());
+        HttpEntity<MultiValueMap<String, String>> requestMessage = new HttpEntity<>(body, httpHeaders);
+        try{
+            ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/signIn/confirm", requestMessage, String.class);
+            String responseBody = response.getBody();
+            if(Objects.equals(responseBody, "NoSearchMember")){
+                bindingResult.reject("loginError","아이디가 일치하지 않습니다.");
+                return "signIn/confirmLogin";
+            }else if(Objects.equals(responseBody, "DifferentPassword")){
+                bindingResult.reject("loginError","비밀번호가 일치하지 않습니다.");
+                return "signIn/confirmLogin";
+            }else{
+                model.addAttribute("userId",loginForm.getId());
+                return "signIn/passwordChangePage";
+            }
+        }catch (Exception e){
+            return "error/searchErrorPage";
+        }
+    }
+
+    @PostMapping("/passwordChange")
+    public String change(@Validated String userId, String password){
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("userId",userId);
+        body.add("password",password);
+        HttpEntity<MultiValueMap<String, String>> requestMessage = new HttpEntity<>(body, httpHeaders);
+        try{
+            restTemplate.postForEntity("http://localhost:8081/signIn/passwordChange",requestMessage,String.class);
+            return "redirect:/";
+        }catch(Exception e){
+            return "error/searchErrorPage";
+        }
     }
 }
